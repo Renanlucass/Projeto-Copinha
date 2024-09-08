4dbd7589920cd1610e438bced95556cccbee1567
Renanlucass <renanlucas0014@gmail.com>
2024-09-08 14:54:06 -0300

    Criando formulário de login

--- components/modal/siginUpModal.js
+++ components/modal/siginUpModal.js
@@ -2,7 +2,8 @@ import React, { useState } from 'react';
 import { Modal, View, Text, TextInput, TouchableOpacity, StyleSheet, Animated } from 'react-native';
 import { Ionicons } from '@expo/vector-icons';
 
-export default function SignUpModal({ visible, onClose }) {
+export default function AuthModal({ visible, onClose }) {
+  const [isLoginMode, setIsLoginMode] = useState(false);
   const [nomeUsuario, setNomeUsuario] = useState('');
   const [email, setEmail] = useState('');
   const [senha, setSenha] = useState('');
@@ -11,6 +12,7 @@ export default function SignUpModal({ visible, onClose }) {
   const [showConfirmSenha, setShowConfirmSenha] = useState(false);
 
   const animation = new Animated.Value(0);
+  const backgroundOpacity = new Animated.Value(0);
 
   React.useEffect(() => {
     if (visible) {
@@ -43,8 +45,6 @@ export default function SignUpModal({ visible, onClose }) {
     }
   }, [visible]);
 
-  const backgroundOpacity = new Animated.Value(0);
-
   const handleClear = () => {
     setNomeUsuario('');
     setEmail('');
@@ -67,6 +67,11 @@ export default function SignUpModal({ visible, onClose }) {
     ]);
   };
 
+  const toggleMode = () => {
+    setIsLoginMode(!isLoginMode);
+    handleClear();
+  };
+
   const scale = animation.interpolate({
     inputRange: [0, 1],
     outputRange: [0.5, 1],
@@ -82,66 +87,116 @@ export default function SignUpModal({ visible, onClose }) {
       <Animated.View style={[styles.modalContainer, { opacity: backgroundOpacity }]}>
         <Animated.View style={[styles.modalContent, { transform: [{ scale }], opacity }]}>
           <View style={styles.header}>
-            <Text style={styles.headerTitleLeft}>Criar Conta</Text>
-            <Text style={styles.headerTitleRight}>Login</Text>
-          </View>
-          <View style={styles.inputContainer}>
-            <Text style={styles.inputLabel}>Nome/Usuário</Text>
-            <TextInput
-              style={styles.input}
-              placeholder="Usuário"
-              value={nomeUsuario}
-              onChangeText={text => setNomeUsuario(text)}
-            />
-          </View>
-          <View style={styles.inputContainer}>
-            <Text style={styles.inputLabel}>Email</Text>
-            <TextInput
-              style={styles.input}
-              placeholder="usuario@gmail.com"
-              value={email}
-              onChangeText={text => setEmail(text)}
-            />
-          </View>
-          <View style={styles.inputContainer}>
-            <Text style={styles.inputLabel}>Senha</Text>
-            <View style={styles.passwordContainer}>
-              <TextInput
-                style={[styles.input, styles.passwordInput]}
-                placeholder="**********"
-                secureTextEntry={!showSenha}
-                value={senha}
-                onChangeText={text => setSenha(text)}
-              />
-              <TouchableOpacity onPress={() => setShowSenha(!showSenha)} style={styles.eyeIcon}>
-                <Ionicons name={showSenha ? 'eye-off' : 'eye'} size={24} color="black" />
-              </TouchableOpacity>
-            </View>
-          </View>
-          <View style={styles.inputContainer}>
-            <Text style={styles.inputLabel}>Confirme a Senha</Text>
-            <View style={styles.passwordContainer}>
-              <TextInput
-                style={[styles.input, styles.passwordInput]}
-                placeholder="**********"
-                secureTextEntry={!showConfirmSenha}
-                value={confirmSenha}
-                onChangeText={text => setConfirmSenha(text)}
-              />
-              <TouchableOpacity onPress={() => setShowConfirmSenha(!showConfirmSenha)} style={styles.eyeIcon}>
-                <Ionicons name={showConfirmSenha ? 'eye-off' : 'eye'} size={24} color="black" />
-              </TouchableOpacity>
-            </View>
-          </View>
-          <View style={styles.buttonsContainer}>
-            <TouchableOpacity style={[styles.button, styles.cadastrarButton]}>
-              <Text style={styles.buttonText}>Cadastrar</Text>
-            </TouchableOpacity>
-            <TouchableOpacity style={[styles.button, styles.limparButton]} onPress={handleClear}>
-              <Text style={styles.buttonText}>Limpar</Text>
+            <Text style={styles.headerTitleLeft}>
+              {isLoginMode ? 'Login' : 'Criar Conta'}
+            </Text>
+            <TouchableOpacity onPress={toggleMode}>
+              <Text style={styles.headerTitleRight}>
+                {isLoginMode ? 'Criar conta' : 'Login'}
+              </Text>
             </TouchableOpacity>
           </View>
+
+          {isLoginMode ? (
+            <>
+              <View style={styles.inputContainer}>
+                <Text style={styles.inputLabel}>Email</Text>
+                <TextInput
+                  style={styles.input}
+                  placeholder="usuario@gmail.com"
+                  value={email}
+                  onChangeText={text => setEmail(text)}
+                />
+              </View>
+              <View style={styles.inputContainer}>
+                <Text style={styles.inputLabel}>Senha</Text>
+                <View style={styles.passwordContainer}>
+                  <TextInput
+                    style={[styles.input, styles.passwordInput]}
+                    placeholder="**********"
+                    secureTextEntry={!showSenha}
+                    value={senha}
+                    onChangeText={text => setSenha(text)}
+                  />
+                  <TouchableOpacity onPress={() => setShowSenha(!showSenha)} style={styles.eyeIcon}>
+                    <Ionicons name={showSenha ? 'eye-off' : 'eye'} size={24} color="black" />
+                  </TouchableOpacity>
+                </View>
+              </View>
+              <View style={styles.buttonsContainer}>
+                <TouchableOpacity style={[styles.button, styles.cadastrarButton]}>
+                  <Text style={styles.buttonText}>Entrar</Text>
+                </TouchableOpacity>
+                <TouchableOpacity style={[styles.button, styles.limparButton]} onPress={handleClear}>
+                  <Text style={styles.buttonText}>Limpar</Text>
+                </TouchableOpacity>
+              </View>
+              <TouchableOpacity>
+                <Text style={styles.forgotPasswordText}>Esqueceu a senha?</Text>
+              </TouchableOpacity>
+            </>
+          ) : (
+            <>
+              <View style={styles.inputContainer}>
+                <Text style={styles.inputLabel}>Nome/Usuário</Text>
+                <TextInput
+                  style={styles.input}
+                  placeholder="Usuário"
+                  value={nomeUsuario}
+                  onChangeText={text => setNomeUsuario(text)}
+                />
+              </View>
+              <View style={styles.inputContainer}>
+                <Text style={styles.inputLabel}>Email</Text>
+                <TextInput
+                  style={styles.input}
+                  placeholder="usuario@gmail.com"
+                  value={email}
+                  onChangeText={text => setEmail(text)}
+                />
+              </View>
+              <View style={styles.inputContainer}>
+                <Text style={styles.inputLabel}>Senha</Text>
+                <View style={styles.passwordContainer}>
+                  <TextInput
+                    style={[styles.input, styles.passwordInput]}
+                    placeholder="**********"
+                    secureTextEntry={!showSenha}
+                    value={senha}
+                    onChangeText={text => setSenha(text)}
+                  />
+                  <TouchableOpacity onPress={() => setShowSenha(!showSenha)} style={styles.eyeIcon}>
+                    <Ionicons name={showSenha ? 'eye-off' : 'eye'} size={24} color="black" />
+                  </TouchableOpacity>
+                </View>
+              </View>
+              <View style={styles.inputContainer}>
+                <Text style={styles.inputLabel}>Confirme a Senha</Text>
+                <View style={styles.passwordContainer}>
+                  <TextInput
+                    style={[styles.input, styles.passwordInput]}
+                    placeholder="**********"
+                    secureTextEntry={!showConfirmSenha}
+                    value={confirmSenha}
+                    onChangeText={text => setConfirmSenha(text)}
+                  />
+                  <TouchableOpacity onPress={() => setShowConfirmSenha(!showConfirmSenha)} style={styles.eyeIcon}>
+                    <Ionicons name={showConfirmSenha ? 'eye-off' : 'eye'} size={24} color="black" />
+                  </TouchableOpacity>
+                </View>
+              </View>
+              <View style={styles.buttonsContainer}>
+                <TouchableOpacity style={[styles.button, styles.cadastrarButton]}>
+                  <Text style={styles.buttonText}>Cadastrar</Text>
+                </TouchableOpacity>
+                <TouchableOpacity style={[styles.button, styles.limparButton]} onPress={handleClear}>
+                  <Text style={styles.buttonText}>Limpar</Text>
+                </TouchableOpacity>
+              </View>
+            </>
+          )}
         </Animated.View>
+
         <Animated.View style={[styles.closeButtonContainer, { opacity }]}>
           <TouchableOpacity onPress={handleModalClose}>
             <View style={styles.closeButtonIconContainer}>
@@ -186,7 +241,6 @@ const styles = StyleSheet.create({
     fontWeight: 'bold',
     color: '#FF8C00',
     textDecorationLine: 'underline',
-    textDecorationColor: '#fff',
   },
   inputContainer: {
     marginBottom: 20,
@@ -215,36 +269,40 @@ const styles = StyleSheet.create({
     flexDirection: 'column',
     justifyContent: 'space-between',
   },
-  button: {
-    padding: 10,
-    width: '60%',
-    borderRadius: 15,
-    alignSelf: 'center',
-    alignItems: 'center',
-    marginVertical: 5,
-  },
   cadastrarButton: {
     backgroundColor: '#041F21',
+    padding: 10,
+    borderRadius: 5,
+    alignItems: 'center',
+    marginBottom: 10,
   },
   limparButton: {
-    backgroundColor: '#041F21',
+    backgroundColor: '#FF8C00',
+    padding: 10,
+    borderRadius: 5,
+    alignItems: 'center',
   },
   buttonText: {
     color: '#fff',
+    fontSize: 16,
     fontWeight: 'bold',
+  },
+  forgotPasswordText: {
+    color: '#041F21',
+    textAlign: 'center',
+    textDecorationLine: 'underline',
+    marginTop: 10,
+  },
+  eyeIcon: {
+    marginLeft: 10,
   },
   closeButtonContainer: {
     position: 'relative',
-    top: 30,
-    zIndex: 1,
+    top: 20,
   },
   closeButtonIconContainer: {
     backgroundColor: '#fff',
-    borderRadius: 25,
-    width: 50,
-    height: 50,
-    justifyContent: 'center',
-    alignItems: 'center',
-    elevation: 5,
+    borderRadius: 20,
+    padding: 10,
   },
 });
